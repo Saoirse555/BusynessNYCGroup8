f01e30035b26af94994d9b8bdcebd20b8437c924
Lou1s-Chan <45930134+Lou1s-Chan@users.noreply.github.com>
2023-07-06 15:52:28 +0100

    Delete demo01 directory

--- demo01/demo/src/main/java/com/example/demo/Service/LocationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Service;
-
-import com.example.demo.Entity.Location;
-
-import java.util.List;
-
-public interface LocationService {
-    public List<Location> getAllLocations();
-}

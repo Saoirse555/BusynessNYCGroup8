2003c2de72db5bc4216a204580411563440a3e01
Itgel Ganbold <itgel98@gmail.com>
2023-08-04 20:13:32 +0100

    Minor styling adjustments

--- demo01/demo/src/frontend/src/components/About.jsx
+++ demo01/demo/src/frontend/src/components/About.jsx
@@ -7,86 +7,86 @@ import Navbar from './Navbar';
 import { HiChevronDoubleDown } from 'react-icons/hi';
 
 const data = [
-    "We are CS Master students at UCD",
-    "This web app is called Auto Mate",
-    "Based on predicted Manhattan road Busyness",
-    "Help drivers plan their routes",
-    "Provide traffic information conveniently"
+    'We are CS Master students at UCD',
+    'This web app is called Auto Mate',
+    'Based on predicted Manhattan road Busyness',
+    'Help drivers plan their routes',
+    'Provide traffic information conveniently'
 ];
 
 const Section = styled.div`
-    background:url("./img/greenbg.jpg");
+    background: url('./img/greenbg.jpg');
     background-size: cover;
-    height:100vh;
-    scroll-snap-align:center;
-    display:felx;
-    justify-content:center;
-    overflow: hidden; 
+    height: 100vh;
+    scroll-snap-align: center;
+    display: felx;
+    justify-content: center;
+    overflow: hidden;
     position: relative;
-`
+`;
 
 const Container = styled.div`
     width: 100%;
-    display:flex;
+    display: flex;
     justify-content: space-between;
-    height:82vh;
+    height: 82vh;
 
     @media only screen and (max-width: 768px) {
         width: 100%;
         flex-direction: column;
-      }
-`
+    }
+`;
 
 const Left = styled.div`
-    display:flex;
-    align-items:center;
-    padding:3vh;
-    width:66%;
+    display: flex;
+    align-items: center;
+    padding: 3vh;
+    width: 66%;
+    font-family: 'Roboto';
 
     @media only screen and (max-width: 768px) {
         padding: 20px;
         justify-content: center;
-      }
-`
+    }
+`;
 
 const Right = styled.div`
-    display:flex;
+    display: flex;
     position: relative;
     flex-direction: column;
-    justify-content:flex-start;
-    width:34%;
-`
+    justify-content: flex-start;
+    width: 34%;
+`;
 
 const List = styled.ul`
-    padding:50px;
-    list-style:none;
-    display:flex;
-    flex-direction:column;
-    gap:20px;
-    top:20%;
-`
+    padding: 50px;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    top: 20%;
+`;
 
 const ListItem = styled.li`
-    font-size:30px;
-    font-weight:bold;
+    font-size: 40px;
+    font-weight: 100;
     color: white;
     -webkit-text-stroke: 1.5px white;
     position: relative;
-    
-    
+
     @media only screen and (max-width: 768px) {
         font-size: 24px;
         color: white;
         -webkit-text-stroke: 0px;
-      }
-    
+    }
+
     &::after {
-        content: "${(props) => props.text}";
+        content: '${(props) => props.text}';
         position: absolute;
         top: 0;
         left: 0;
-        background-color:#afeeee ;
-        // background-image: url(./img/NYC.jpg); 
+        background-color: #afeeee;
+        // background-image: url(./img/NYC.jpg);
         // background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
@@ -95,16 +95,16 @@ const ListItem = styled.li`
         overflow: hidden;
         white-space: nowrap;
         transition: width 0.5s;
-        }
+    }
 
     &:hover::after {
         width: 100%;
-        }    
+    }
 
     @media only screen and (max-width: 768px) {
         justify-content: center;
-        font-size:1.3rem;
-        }
+        font-size: 1.3rem;
+    }
 `;
 
 const moveDown = keyframes`
@@ -120,54 +120,52 @@ const moveDown = keyframes`
 `;
 
 const AnimatedHiChevronDoubleDown = styled(HiChevronDoubleDown)`
-  height: 3vh;
-  animation: ${moveDown} 2s infinite;
-  position: relative;
-  left: 50%;
-  transform: translateX(-50%);
-  zoom: 1.5; 
-  color: white;
-  z-index:999;
-  bottom:25px;
+    height: 3vh;
+    animation: ${moveDown} 2s infinite;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    zoom: 1.5;
+    color: white;
+    z-index: 999;
+    bottom: 25px;
 
+    @media screen and (max-width: 900px) {
+        white-space: normal;
+        font-size: 3rem;
+    }
 
-  @media screen and (max-width: 900px) {
-    white-space: normal;
-    font-size: 3rem;
-    }  
-
-   @media screen and (max-width: 760px) {
+    @media screen and (max-width: 760px) {
         font-size: 1.5rem;
     }
 `;
 
-
-const About = () =>{
-    return( 
-            <Section id="about">
-                <Navbar/>
-                <Container>
-                    <Left>
-                        <List>
-                            {data.map((item)=>(
-                                <ListItem key={item} text={item}>
-                                    {item}
-                                </ListItem>))}
-                        </List>
-                    </Left>
-                    <Right>
+const About = () => {
+    return (
+        <Section id="about">
+            <Navbar />
+            <Container>
+                <Left>
+                    <List>
+                        {data.map((item) => (
+                            <ListItem key={item} text={item}>
+                                {item}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Left>
+                <Right>
                     <Canvas>
                         <OrbitControls enableZoom={false} />
-                        <ambientLight intensity= {2}/>
-                        <directionalLight position={[3,2,1]}/>
-                            <Cube />
+                        <ambientLight intensity={2} />
+                        <directionalLight position={[3, 2, 1]} />
+                        <Cube />
                     </Canvas>
-                    </Right>
-                </Container>
-                <AnimatedHiChevronDoubleDown />
-            </Section>
-
-    )
-}
+                </Right>
+            </Container>
+            <AnimatedHiChevronDoubleDown />
+        </Section>
+    );
+};
 
 export default About;

6d8a2dc9c0b32caa7e74ab381dfbf25e1807bce2
Lou1s-Chan <tchen441@gmail.com>
2023-08-16 13:38:55 +0100

    Revert "Resolve merge conflicts"
    This reverts commit cd7e8e712820a9ef0d43135dd0afd38aa94b4ea7, reversing
    changes made to de5656cdd0fe59e82076b74c5566a497639c551a.

---
+++ demo01/demo/src/main/java/com/example/demo/Controller/LocationController.java
@@ -0,0 +1,10 @@
+package com.example.demo.Controller;
+
+
+import com.example.demo.Entity.Location;
+
+import java.util.List;
+
+public interface LocationController {
+    List<Location> getAllLocation();
+}
